34bd146c96c753123fd674004f2b84f89e0d70b6
Ion Ander <iagurrutxaga@iketek.net>
2016-02-12 13:57:52 +0100

    Added square function

--- test/sicp_clojure/core_test.clj
+++ test/sicp_clojure/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [sicp-clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest square-2-test
+  (testing "The square of 2 is 4."
+    (is (= 4 (square 2)))))
+
+(deftest square-3-test
+  (testing "The square of 3 is 9."
+    (is (= 9 (square 3)))))
